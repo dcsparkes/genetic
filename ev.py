1e7024b2165a333110a70ca56587f601516acd93
dcsparkes@gmail.com <cwBA44K4jAcqHQ5>
2021-04-12 05:15:44 +0100

    Knock together calculator to estimate length of pixel chains for a given crossover probability.

---
+++ ev.py
@@ -0,0 +1,17 @@
+def calculateExpected(probability, places):
+    """Calculate limit of probability function... probably a way to do it mathematically"""
+    expectedValue = 0
+    increase = 1
+    tolerance = 0.1 ** places
+    x = 1
+    while increase > tolerance:
+        increase = x * probability * (1 - probability) ** (x - 1)
+        expectedValue += increase
+        print("Iteration: {}, Expected: {}, Increase: {}".format(x, expectedValue, increase))
+        x += 1
+    return expectedValue
+
+
+if __name__ == '__main__':
+    print("Expected excerpt length = {} pixels".format(calculateExpected(0.0001, 18)))
+
